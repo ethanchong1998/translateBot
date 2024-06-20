65d7550b4a8da6c62b699d645cb64e1bf45c2050
Ethan Chong <ethanchong@Ethans-MacBook-Air.local>
2024-06-20 17:22:32 +0800

    optimize extracted link

--- app/config/channel_rules.py
+++ app/config/channel_rules.py
@@ -1,3 +1,7 @@
 def top7ico_rules(msg: str):
-    clean_msg = msg.replace("\U0001F517", "--")
+    clean_msg = msg.replace("\U0001F517", "")
+    return clean_msg
+
+def cryptodiffer_rules(msg: str):
+    clean_msg = msg.replace("\U0001F449", "")
     return clean_msg
